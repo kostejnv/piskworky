7f8bbb95419b0259a6f6aa9d5173ae32500b02d8
flgn <v.kostejn.vk@gmail.com>
2021-02-03 20:38:23 +0100

    communication with console

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <ncurses.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    initscr();
+    WINDOW * win = newwin(1000, 1000, 0, 0);
+    noecho();
+    int x = 0;
+    int y = 0;
+    while (true) {
+
+        char c = getch();
+
+        switch (c) {
+            // normal character handling
+            case 10:
+                waddch(win, 'X');
+                wmove(win, y, x);
+                break;
+            case 'w':
+                y -= 2;
+                wmove(win, y, x);
+                break;
+            case 's':
+                y += 2;
+                wmove(win, y, x);
+                break;
+            case 'd':
+                x += 2;
+                wmove(win, y, x);
+                break;
+            case 'a':
+                x -= 2;
+                wmove(win, y, x);
+                break;
+            default:
+                break;
+
+        }
+        wrefresh(win);
+    }
+
+
     return 0;
 }
