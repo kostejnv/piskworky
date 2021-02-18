cc5f2533d4bf402b49c708e1daabde8790fff99f
flgn <v.kostejn.vk@gmail.com>
2021-02-18 15:47:24 +0100

    create point class

---
+++ point.h
@@ -0,0 +1,17 @@
+//
+// Created by flgn on 18.02.21.
+//
+
+#ifndef PISKWORKY_POINT_H
+#define PISKWORKY_POINT_H
+
+class point{
+public:
+    int x, y;
+    point(int x1, int y1){
+        x = x1;
+        y = y1;
+    }
+};
+
+#endif //PISKWORKY_POINT_H
